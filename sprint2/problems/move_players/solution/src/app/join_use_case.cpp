bd1c7d743c6967be7904d626a80df82021c81c40
Maxim Mitsura <mnmaximus@yandex.ru>
2023-11-23 16:47:59 +0300

    Добавил файлы в папку с решением

---
+++ sprint2/problems/move_players/solution/src/app/join_use_case.cpp
@@ -0,0 +1,71 @@
+#include "join_use_case.h"
+#include "model.h"
+#include <cstddef>
+#include <stdexcept>
+
+namespace app {
+
+// Проверка имени на правильность
+bool isValidName(Player::Name name) {
+    // На данный момент проверяем только пустоту строки
+    return !(*name).empty();
+}
+
+// Получение произвольной точки на дорогах карты
+model::Position GetRandomPointOnMap(const model::Map& map) {
+    auto roads = map.GetRoads();
+    // Выбираем случайную дорогу
+    size_t road_index = utils::my_random::GetRandomIndex(0, roads.size()-1);
+    model::Road& road = roads[road_index];
+
+    // Выбираем случайную точку на дороге
+    model::Point start = road.GetStart();
+    model::Point end = road.GetEnd();
+
+    model::Position res;
+    if ( road.IsHorizontal() ) {
+        res.y = start.y;
+        res.x = utils::my_random::GetRandomDouble(start.x, end.x);
+    } else {
+        res.x = start.x;
+        res.y = utils::my_random::GetRandomDouble(start.y, end.y);
+    }
+
+    return res;
+}
+
+JoinGameUseCase::JoinGameUseCase(model::Game& game, PlayerTokens& player_tokens, Players& players) 
+    : game_{&game}
+    , player_tokens_{&player_tokens}
+    , players_{&players} {
+}
+
+// Подключает игрока с указанным именем (пса) к указанной карте
+JoinGameResult JoinGameUseCase::JoinGame(model::Map::Id map_id, Player::Name name) {
+    model::Dog::Name name_str(*name);
+    if ( !isValidName(name) ) {
+        throw JoinGameError{JoinGameErrorReason::InvalidName};
+    }
+
+    auto map = game_->FindMap(map_id);
+    if ( !map ) {
+        throw JoinGameError{JoinGameErrorReason::InvalidMap};
+    }
+    
+    if ( auto session = game_->FindSession(model::Map::Id{map_id}) ) {
+        auto spawn_point = GetRandomPointOnMap(*map);
+        try {
+            auto dog = session->AddDog(spawn_point, std::move(name_str));
+            auto& player = players_->Add(dog, *session);
+            auto token = player_tokens_->AddPlayer(player);
+            return {token, player.GetId()};
+        }
+        catch ( std::invalid_argument err ) {
+            throw JoinGameError{JoinGameErrorReason::InvalidName};
+        }
+    }
+    throw JoinGameError{JoinGameErrorReason::InvalidMap};
+}
+
+
+}  // namespace app
---
+++ sprint2/problems/move_players/solution/src/http/http_handler_types.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <boost/json/conversion.hpp>
+#include <boost/beast/http.hpp>
+
+namespace http_handler {
+
+class JoinParams {
+public:
+    std::string name;
+    std::string map_id;
+};
+
+namespace beast = boost::beast;
+namespace http = beast::http;
+
+// Запрос, тело которого представлено в виде строки
+using StringRequest = http::request<http::string_body>;
+// Ответ, тело которого представлено в виде строки
+using StringResponse = http::response<http::string_body>;
+
+// Ответ, тело которого представлено в виде бинарной последовательности
+using FileResponse = http::response<http::file_body>;
+// @todo
+// using FileRequestResult = std::variant<EmptyResponse, StringResponse, FileResponse>;
+using FileRequestResult = std::variant<StringResponse, FileResponse>;
+
+struct ResponseError {
+    std::string code;
+    std::string message;
+};
+
+// сериализация экземпляра класса ResponseError в JSON-значение
+void tag_invoke(boost::json::value_from_tag, boost::json::value& jv, http_handler::ResponseError const& resp_err);
+
+}  // namespace http_handler
