97e43160e880ebd84df41ba4475eed28cfbfbb28
Maxim Mitsura <mnmaximus@yandex.ru>
2023-11-24 12:21:08 +0300

    Добавил файлы для работы над задачей

---
+++ sprint2/problems/time_control/solution/src/app/state_use_case.cpp
@@ -0,0 +1,28 @@
+#include "state_use_case.h"
+#include <stdexcept>
+
+namespace app {
+
+// Для игрока с указанным токеном выдаёт список игроков, которые находятся вместе с ним в одной сессии
+GetStateResult GetStateUseCase::GetState(Token token) {
+    GetStateResult res;
+    // Получаем игрока с заданным токеном
+    if ( auto self_player = player_tokens_->FindPlayerByToken(token) ) {
+        // Получаем сессию, к которой подключен игрок и список собак в сессии
+        auto session = self_player->GetSession();
+        auto dogs = session->GetDogs();;
+
+        // Для каждой собаки находим игрока и складываем в результат
+        for ( auto dog : dogs ) {
+            auto dog_name = dog->GetName();
+            auto player = players_->FinByDog(*dog);
+            res.push_back({player->GetId(), *dog});
+        }
+    } else {
+        throw GetStateError{GetStateErrorReason::InvalidToken};
+    }
+
+    return res;
+}
+
+}  // namespace app
