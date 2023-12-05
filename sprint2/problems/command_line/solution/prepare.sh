ce109eaa79dab242376512688ec89eb8ad455d52
Maxim Mitsura <mnmaximus@yandex.ru>
2023-12-05 12:05:01 +0300

    Починил запуск сервера в контейнере: исправлены аргументы

--- sprint2/problems/command_line/solution/prepare.sh
+++ sprint2/problems/command_line/solution/prepare.sh
@@ -6,3 +6,13 @@ mkdir -p build && cd build && \
 conan profile update settings.compiler.libcxx=libstdc++11 default && \
 conan install .. --build=missing && \
 cd ..
+
+# mkdir -p build-release && cd build-release
+# conan install .. --build=missing -s build_type=Release -s compiler.libcxx=libstdc++11
+# cmake .. -DCMAKE_BUILD_TYPE=Release
+# cd ..
+
+# mkdir -p build-debug && cd build-debug
+# conan install .. --build=missing -s build_type=Debug -s compiler.libcxx=libstdc++11
+# cmake .. -DCMAKE_BUILD_TYPE=Debug
+# cd ..
