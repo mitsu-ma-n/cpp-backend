1b5547f1a194482e1829afaec60dfaba8a9fcbad
Maxim Mitsura <mnmaximus@yandex.ru>
2023-12-11 12:17:10 +0300

    Декодирование URL

--- sprint3/problems/urldecode/solution/src/urldecode.cpp
+++ sprint3/problems/urldecode/solution/src/urldecode.cpp
@@ -1,22 +1,62 @@
 #include "urldecode.h"
 
+#include <algorithm>
 #include <charconv>
+#include <iomanip>
 #include <stdexcept>
+#include <regex>
 
 #include <set>
 #include <sstream>
+#include <iostream>
 
 using namespace std::literals;
 
-std::string to_hex(char c) {
+template<typename T>
+std::string to_hex(T c) {
     std::stringstream ss;
-    ss << std::hex << c;
+    ss << '%' << std::hex << std::setw(2) << std::setfill('0') << static_cast<unsigned>(c);
+    std::cout << "char: " << c << " , string: " << ss.str() << std::endl;
     return ss.str();
 }
 
+
 std::string UrlDecode(std::string_view str) {
+    std::string result;
+    result.reserve(str.size());
+    
+    for (std::size_t i = 0; i < str.size(); ++i)
+    {
+        auto ch = str[i];
+        
+        if (ch == '%' && (i + 2) < str.size())
+        {
+            std::string hex{str.substr(i + 1, 2)};
+            std::transform(hex.begin(), hex.end(), hex.begin(), ::toupper);
+            std::smatch sm;
+            if( !std::regex_match(hex, sm, std::regex("[0-9A-F]{2}")) ) {
+                throw std::invalid_argument("Invalid URL encoding");
+            }
+            auto dec = static_cast<char>(std::strtol(hex.c_str(), nullptr, 16));
+            result.push_back(dec);
+            i += 2;
+        }
+        else if (ch == '+')
+        {
+            result.push_back(' ');
+        }
+        else
+        {
+            result.push_back(ch);
+        }
+    }
+    
+    return result;
+}
+
+std::string UrlEncode(std::string_view str) {
     const char tmp[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.~";
-    std::set<char> not_decode{tmp, tmp + sizeof(tmp) - 1}; 
+    std::set<char> not_encode{tmp, tmp + sizeof(tmp) - 1}; 
 
     std::string res;
 
@@ -24,11 +64,12 @@ std::string UrlDecode(std::string_view str) {
     // запускаем цикл, пока не кончится строка
     while (st != str.end()) {
         // Если символ в списке допустимых
-        if( not_decode.contains(*st) ) { // тогда копируем его как есть
+        if( not_encode.contains(*st) ) { // тогда копируем его как есть
             res.push_back(*st);
         } else {    // Преобразуем
             res += to_hex(*st);
         }
+        ++st;
     }
 
     return res;
--- sprint3/problems/urldecode/solution/tests/tests.cpp
+++ sprint3/problems/urldecode/solution/tests/tests.cpp
@@ -6,6 +6,17 @@
 BOOST_AUTO_TEST_CASE(UrlDecode_tests) {
     using namespace std::literals;
 
+    // Пустая строка.
     BOOST_TEST(UrlDecode(""sv) == ""s);
+    // Строка без %-последовательностей.
+    BOOST_TEST(UrlDecode("https://vk.com/im?sel=c1"sv) == "https://vk.com/im?sel=c1"s);
+    // Строка с валидными %-последовательностями, записанными в разном регистре.
+    BOOST_TEST(UrlDecode("https://ru.wikipedia.org/wiki/%D0%97%D0%B0%d0%B3%D0%Bb%D0%b0%D0%B2%D0%bD%D0%B0%D1%8f%D0%bb"sv) == "https://ru.wikipedia.org/wiki/Заглавнаял"s);
+    // Строка с невалидными %-последовательностями.
+    BOOST_CHECK_THROW(UrlDecode("wiki/%LL%97%DX%B0"sv), std::invalid_argument);
+    // Строка с неполными %-последовательностями.
+    BOOST_CHECK_THROW(UrlDecode("wiki/%D%97%"sv), std::invalid_argument);
+    // Строка с символом +.
+    BOOST_TEST(UrlDecode("https://ru.wikipedia.org/wiki/%D0%97+%D0%97"sv) == "https://ru.wikipedia.org/wiki/З З"s);
     // Напишите остальные тесты для функции UrlDecode самостоятельно
 }
