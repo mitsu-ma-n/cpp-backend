1f87f414c24933f702b08fb1ce376bad15aa9429
Maxim Mitsura <mnmaximus@yandex.ru>
2023-12-19 07:56:11 +0300

    Вернул gather-tests к чистому состоянию

--- sprint3/problems/gather-tests/solution/src/collision_detector.h
+++ sprint3/problems/gather-tests/solution/src/collision_detector.h
@@ -12,13 +12,15 @@ struct CollectionResult {
         return proj_ratio >= 0 && proj_ratio <= 1 && sq_distance <= collect_radius * collect_radius;
     }
 
-    // Квадрат расстояния до точки
+    // квадрат расстояния до точки
     double sq_distance;
-    // Доля пройденного отрезка
+
+    // доля пройденного отрезка
     double proj_ratio;
 };
 
-// Движемся из точки a в точку b и пытаемся подобрать точку c
+// Движемся из точки a в точку b и пытаемся подобрать точку c.
+// Эта функция реализована в уроке.
 CollectionResult TryCollectPoint(geom::Point2D a, geom::Point2D b, geom::Point2D c);
 
 struct Item {
@@ -50,11 +52,8 @@ struct GatheringEvent {
     double time;
 };
 
+// Эту функцию вам нужно будет реализовать в соответствующем задании.
+// При проверке ваших тестов она не нужна - функция будет линковаться снаружи.
 std::vector<GatheringEvent> FindGatherEvents(const ItemGathererProvider& provider);
-std::vector<GatheringEvent> FindGatherEvents_Wrong1(const ItemGathererProvider& provider);
-std::vector<GatheringEvent> FindGatherEvents_Wrong2(const ItemGathererProvider& provider);
-std::vector<GatheringEvent> FindGatherEvents_Wrong3(const ItemGathererProvider& provider);
-std::vector<GatheringEvent> FindGatherEvents_Wrong4(const ItemGathererProvider& provider);
-std::vector<GatheringEvent> FindGatherEvents_Wrong5(const ItemGathererProvider& provider);
 
 }  // namespace collision_detector
