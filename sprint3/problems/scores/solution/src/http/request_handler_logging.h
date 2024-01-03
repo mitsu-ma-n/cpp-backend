8b31b3506142c9fe9f7628357da14ccc7cc233eb
Maxim Mitsura <mnmaximus@yandex.ru>
2024-01-03 18:56:06 +0300

    Файлы для scores под контроль ревизий

---
+++ sprint3/problems/scores/solution/src/http/request_handler_logging.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <boost/log/trivial.hpp>     // для BOOST_LOG_TRIVIAL
+
+#include "json_fields.h"
+#include "request_handler.h"
+#include <boost/json.hpp>
+#include <boost/json/value.hpp>
+#include <boost/asio/ip/tcp.hpp>
+
+#include <boost/timer/timer.hpp>
+#include <memory>
+
+#include "server_params.h"
+#include "logger.h"
+
+namespace http_handler {
+
+template<class SomeRequestHandler>
+class LoggingRequestHandler {
+public:
+    LoggingRequestHandler(SomeRequestHandler hend) : decorated_(hend) {}
+
+    template <typename Body, typename Allocator, typename Send>
+    void operator()(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send, boost::asio::ip::tcp::endpoint&& endpoint) {
+        // Формируем объект с информацией о запросе и печатаем
+        boost::json::object request_jobject;
+        request_jobject[json_field::REQUEST_IP] = endpoint.address().to_string();
+        request_jobject[json_field::REQUEST_URI] = std::string(req.target());
+        request_jobject[json_field::REQUEST_METHOD] = req.method_string();
+
+        BOOST_LOG_TRIVIAL(info)  << boost::log::add_value(additional_data, boost::json::value(request_jobject)) 
+                                 << server_params::REQUEST_RECEIV_MESSAGE;
+
+        // Заводим объект под информацию об ответе
+        auto response_jobject = std::make_shared<boost::json::object>();
+
+        // Заводим таймер, чтобы засечь время формирования ответа
+        auto timer = std::make_shared<boost::timer::cpu_timer>();
+
+        auto loggingResponse = [send, response_jobject, timer](auto&& response) {
+            timer->stop();
+            boost::timer::cpu_times times = timer->elapsed();
+
+            // Заполняем информацию об ответе. Обработка запроса к этому времени уже произведена
+            (*response_jobject)[json_field::RESPONSE_TIME] = times.wall / 1'000'000;   // наносекунды в миллисекунды
+            (*response_jobject)[json_field::RESPONSE_CODE] = response.result_int();
+            (*response_jobject)[json_field::RESPONSE_CONTENT_TYPE] = response[http::field::content_type];
+
+            // непосредственная отправка ответа
+            send(response);
+
+            BOOST_LOG_TRIVIAL(info) << boost::log::add_value(additional_data, boost::json::value(*response_jobject))
+                                    << server_params::RESPONSE_SENT_MESSAGE;
+
+        };
+
+        // Непосредственно обработка запроса с использованием лямбды
+        decorated_(std::forward<decltype(req)>(req), std::move(loggingResponse));
+    }
+
+private:
+     SomeRequestHandler decorated_;
+};
+
+}  // namespace http_handler
---
+++ sprint3/problems/scores/solution/tests/collision-detector-tests.cpp
@@ -0,0 +1,137 @@
+#define _USE_MATH_DEFINES
+
+#include <cmath>
+#include <functional>
+#include <sstream>
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_templated.hpp>
+
+#include "../src/utils/collision_detector.h"
+
+namespace Catch {
+template<>
+struct StringMaker<collision_detector::GatheringEvent> {
+    static std::string convert(collision_detector::GatheringEvent const& value) {
+        std::ostringstream tmp;
+        tmp << "(" << value.gatherer_id << value.item_id << value.sq_distance << value.time << ")";
+
+        return tmp.str();
+    }
+};
+}  // namespace Catch
+
+namespace {
+
+template <typename Range, typename Predicate>
+struct EqualsRangeMatcher : Catch::Matchers::MatcherGenericBase {
+    EqualsRangeMatcher(Range const& range, Predicate predicate)
+        : range_{range}
+        , predicate_{predicate} {
+    }
+
+    template <typename OtherRange>
+    bool match(const OtherRange& other) const {
+        using std::begin;
+        using std::end;
+
+        return std::equal(begin(range_), end(range_), begin(other), end(other), predicate_);
+    }
+
+    std::string describe() const override {
+        return "Equals: " + Catch::rangeToString(range_);
+    }
+
+private:
+    const Range& range_;
+    Predicate predicate_;
+};
+
+template <typename Range, typename Predicate>
+auto EqualsRange(const Range& range, Predicate prediate) {
+    return EqualsRangeMatcher<Range, Predicate>{range, prediate};
+}
+
+}
+
+SCENARIO("Collision detection") {
+    WHEN("no items") {
+        collision_detector::VectorItemGathererProvider provider{
+            {}, {{{1, 2}, {4, 2}, 5.}, {{0, 0}, {10, 10}, 5.}, {{-5, 0}, {10, 5}, 5.}}};
+        THEN("No events") {
+            auto events = collision_detector::FindGatherEvents(provider);
+            CHECK(events.empty());
+        }
+    }
+    WHEN("no gatherers") {
+        collision_detector::VectorItemGathererProvider provider{
+            {{0, {1, 2}, 5.}, {1, {0, 0}, 5.}, {2, {-5, 0}, 5.}}, {}};
+        THEN("No events") {
+            auto events = collision_detector::FindGatherEvents(provider);
+            CHECK(events.empty());
+        }
+    }
+    WHEN("multiple items on a way of gatherer") {
+        collision_detector::VectorItemGathererProvider provider{{
+            {0, {9, 0.27}, .1},
+            {1, {8, 0.24}, .1},
+            {2, {7, 0.21}, .1},
+            {3, {6, 0.18}, .1},
+            {4, {5, 0.15}, .1},
+            {5, {4, 0.12}, .1},
+            {6, {3, 0.09}, .1},
+            {7, {2, 0.06}, .1},
+            {8, {1, 0.03}, .1},
+            {9, {0, 0.0}, .1},
+            {10, {-1, 0}, .1},
+            }, {
+            {{0, 0}, {10, 0}, 0.1},
+        }};
+        THEN("Gathered items in right order") {
+            auto events = collision_detector::FindGatherEvents(provider);
+            CHECK_THAT(
+                events,
+                EqualsRange(std::vector{
+                    collision_detector::GatheringEvent{9, 0,0.*0., 0.0},
+                    collision_detector::GatheringEvent{8, 0,0.03*0.03, 0.1},
+                    collision_detector::GatheringEvent{7, 0,0.06*0.06, 0.2},
+                    collision_detector::GatheringEvent{6, 0,0.09*0.09, 0.3},
+                    collision_detector::GatheringEvent{5, 0,0.12*0.12, 0.4},
+                    collision_detector::GatheringEvent{4, 0,0.15*0.15, 0.5},
+                    collision_detector::GatheringEvent{3, 0,0.18*0.18, 0.6},
+                }, collision_detector::CompareEvents()));
+        }
+    }
+    WHEN("multiple gatherers and one item") {
+        collision_detector::VectorItemGathererProvider provider{{
+                                                {0, {0, 0}, 0.},
+                                            },
+                                            {
+                                                {{-5, 0}, {5, 0}, 1.},
+                                                {{0, 1}, {0, -1}, 1.},
+                                                {{-10, 10}, {101, -100}, 0.5}, // <-- that one
+                                                {{-100, 100}, {10, -10}, 0.5},
+                                            }
+        };
+        THEN("Item gathered by faster gatherer") {
+            auto events = collision_detector::FindGatherEvents(provider);
+            CHECK(events.front().gatherer_id == 2);
+        }
+    }
+    WHEN("Gatherers stay put") {
+        collision_detector::VectorItemGathererProvider provider{{
+                                                {0, {0, 0}, 10.},
+                                            },
+                                            {
+                                                {{-5, 0}, {-5, 0}, 1.},
+                                                {{0, 0}, {0, 0}, 1.},
+                                                {{-10, 10}, {-10, 10}, 100}
+                                            }
+        };
+        THEN("No events detected") {
+            auto events = collision_detector::FindGatherEvents(provider);
+
+            CHECK(events.empty());
+        }
+    }
+}
